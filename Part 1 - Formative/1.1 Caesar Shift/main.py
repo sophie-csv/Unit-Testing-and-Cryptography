bad23ff419a90744cdc1ee8ebbc715aef3f5ebc3
Sophie Castellon <2025castellon.sophie@fcboe.org>
2024-09-04 11:11:20 -0400

    finished part 1, init commit

--- Part 1 - Formative/1.1 Caesar Shift/main.py
+++ Part 1 - Formative/1.1 Caesar Shift/main.py
@@ -4,17 +4,31 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def caesar_encode(text, n):
-    return ""
+    temp = ""
+    for let in text:
+        index = alpha.index(let)
+        temp += alpha[index + n] % 26
+    return temp
+
+"""    for i in range(len(text)):
+        for y in range(len(alpha)):
+            if text[i] == alpha[y]:
+                if y > len(alpha) - n:
+                    temp += alpha[n - 26 % y]
+                else:
+                    temp += alpha[y + n]
+    return temp"""
 
 
-def caesar_decode(text, n):
-    return ""
+
+#def caesar_decode(text, n):
+#    return ""
 
 
 test = "HELLOWORLD"
 shift = 5
 enc = caesar_encode(test, shift)
-dec = caesar_decode(enc, shift)
+#dec = caesar_decode(enc, shift)
 print(enc)
-print(dec)
+#print(dec)
 # If this worked, dec should be the same as test!
