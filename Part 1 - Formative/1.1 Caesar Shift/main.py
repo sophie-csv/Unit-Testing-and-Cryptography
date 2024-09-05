a6ca01d87d0cec38b6a118f53c1fe9909e746b3f
Sophie Castellon <2025castellon.sophie@fcboe.org>
2024-09-05 10:38:06 -0400

    CCompleted part 1.1 (Encode & Decode)

--- Part 1 - Formative/1.1 Caesar Shift/main.py
+++ Part 1 - Formative/1.1 Caesar Shift/main.py
@@ -16,14 +16,22 @@ def caesar_encode(text, n):
 
 
 
-#def caesar_decode(text, n):
-#    return ""
+def caesar_decode(text, n):
+    temp = ""
+    for i in range(len(text)):
+        for y in range(len(alpha)):
+            if text[i] == alpha[y]:
+                if y > len(alpha) + n:
+                    temp += alpha[n - 26 % y]
+                else:
+                    temp += alpha[y - n]
+    return temp
 
 
 test = "HELLOWORLD"
 shift = 5
 enc = caesar_encode(test, shift)
-#dec = caesar_decode(enc, shift)
+dec = caesar_decode(enc, shift)
 print(enc)
-#print(dec)
-# If this worked, dec should be the same as test!
+print(dec)
+#If this worked, dec should be the same as test!
