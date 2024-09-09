a2b21f32f2da7a2c12ef589ef07cbfe9e92584e0
Sophie Castellon <2025castellon.sophie@fcboe.org>
2024-09-09 11:10:54 -0400

    EOD 9/9/2024

--- Part 1 - Formative/1.2 Substitution Cipher/main.py
+++ Part 1 - Formative/1.2 Substitution Cipher/main.py
@@ -1,11 +1,16 @@
 # Read the instructions to see what you need to do here!
 
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|:"<>?'
 
 
 def sub_encode(text, codebet):
+    if text.isdigit() or len(text) > len(codebet):
+        return text
+    text = text.upper()
     temp = ""
     for i in range(len(text)):
+        if text[i] == " ":
+            temp += " "
         for y in range(len(alpha)):
             if text[i] == alpha[y]:
                 temp += cipher_alphabet[y]
@@ -15,6 +20,8 @@ def sub_encode(text, codebet):
 def sub_decode(text, codebet):
     temp = ""
     for i in range(len(text)):
+        if text[i] == " ":
+            temp += " "
         for y in range(len(alpha)):
             if text[i] == cipher_alphabet[y]:
                 temp += alpha[y]
@@ -22,7 +29,7 @@ def sub_decode(text, codebet):
 
 
 test = "HELLOWORLD"
-cipher_alphabet = "WJKUXVBMIYDTPLHZGONCRSAEFQ"
+cipher_alphabet = 'WJKUXVBMIYDTPLHZGONCRSAEFQ?"><:|}{+_)(*&^%$#@!)}'
 enc = sub_encode(test, cipher_alphabet)
 dec = sub_decode(enc, cipher_alphabet)
 print(enc)
---
+++ Part 1 - Formative/1.2 Substitution Cipher/tests/test_sub_encode.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import sub_encode
+
+class TestCaesarDecode(TestCase):
+    cipher_alphabet = "WJKUXVBMIYDTPLHZGONCRSAEFQ"
+    cipher_alphabet_with_punc = 'WJKUXVBMIYDTPLHZGONCRSAEFQ?"><:|}{+_)(*&^%$#@!)}'
+    def test_sub_encode_with_spaces(self):
+        self.assertEqual(sub_encode("TE XT", self.cipher_alphabet), "CX EC")
+    def test_sub_encode_with_lower(self):
+        self.assertEqual(sub_encode("text", self.cipher_alphabet), "CXEC")
+    def test_sub_encode_with_numbers(self):
+        self.assertEqual(sub_encode("3h", self.cipher_alphabet), "3h")
+
+    def test_sub_encode_with_none(self):
+        self.assertEqual(sub_encode("", self.cipher_alphabet), "")
+
+    def test_sub_encode_with_switched(self):
+        self.assertEqual(sub_encode(self.cipher_alphabet, "HELLO"), "WJKUXVBMIYDTPLHZGONCRSAEFQ")
+
+    def test_sub_encode_with_punctuation(self):
+        self.assertEqual(sub_encode("HELLO!",self.cipher_alphabet ), "MXTTH?")
+    def test_sub_encode_with_punctuation(self):
+        self.assertEqual(sub_encode("HELLO!", self.cipher_alphabet_with_punc), "MXTTH?")
+
+    def test_sub_encode_with_everything(self):
+        self.assertEqual(sub_encode("Texx3!(& thankS", self.cipher_alphabet_with_punc), "CXEE?+} CMWLDN")
