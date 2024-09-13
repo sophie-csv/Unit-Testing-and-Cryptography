49fb9635d88092813459dc39a4f98157539dd43b
Sophie Castellon <2025castellon.sophie@fcboe.org>
2024-09-13 10:54:35 -0400

    Completed 2.1

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -3,12 +3,27 @@
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # go down the column and add the index of t + t index and find the sum from the alpha
 def vig_encode(text, keyword):
-
-  return ""
+  temp = ""
+  for i in range(len(text)):
+    for y in range(len(alpha)):
+      if text[i] == alpha[y]:
+        index1 = y
+        index2 = alpha.index(keyword[i % len(keyword)])
+        letter = alpha[(index1 + index2) % 26]
+        temp += letter
+  return temp
 
 
 def vig_decode(text, keyword):
-  return ""
+  temp = ""
+  for i in range(len(text)):
+    for y in range(len(alpha)):
+      if text[i] == alpha[y]:
+        index1 = y
+        index2 = alpha.index(keyword[i % len(keyword)])
+        letter = alpha[(index1 - index2) % 26]
+        temp += letter
+  return temp
 
 
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
