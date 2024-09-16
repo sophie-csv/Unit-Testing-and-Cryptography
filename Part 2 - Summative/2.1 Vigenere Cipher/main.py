c2ec38dce46476000077238f328cf8db2f46ed2a
Sophie Castellon <2025castellon.sophie@fcboe.org>
2024-09-16 11:10:43 -0400

    EOD 9/16/2024 Completed tests for vig encode.

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -1,8 +1,9 @@
 # Read the instructions to see what you need to do here!
 
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 # go down the column and add the index of t + t index and find the sum from the alpha
 def vig_encode(text, keyword):
+  text = text.upper()
   """
   Uses the keyword to encode text. Will take the index of both and add it to find corresponding alpha index.
   :param text: The text to be encoded
@@ -11,16 +12,23 @@ def vig_encode(text, keyword):
   """
   temp = ""
   for i in range(len(text)):
-    for y in range(len(alpha)):
-      if text[i] == alpha[y]:
-        index1 = y
+    if text[i] in alpha:
+        index1 = alpha.index(text[i])
         index2 = alpha.index(keyword[i % len(keyword)])
-        letter = alpha[(index1 + index2) % 26]
+        letter = alpha[(index1 + index2) % 27]
         temp += letter
+    else:
+      temp += text[i]
+
+
+
   return temp
 
 
 def vig_decode(text, keyword):
+  if text.isdigit():
+    return text
+  text = text.upper()
   """
   Decodes the text. Will take the index of both and add it to find corresponding alpha index.
   :param text: the text to be decoded
@@ -33,13 +41,13 @@ def vig_decode(text, keyword):
       if text[i] == alpha[y]:
         index1 = y
         index2 = alpha.index(keyword[i % len(keyword)])
-        letter = alpha[(index1 - index2) % 26]
+        letter = alpha[(index1 - index2) % 27]
         temp += letter
   return temp
 
 
-test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
-vig_key = "TEST"
+test = "TEST"
+vig_key = "THE QUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
 enc = vig_encode(test, vig_key)
 dec = vig_decode(enc, vig_key)
 print(enc)
